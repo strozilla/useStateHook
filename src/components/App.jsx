2cea1ce0d049db5e2f63849e77910895d79d348d
Castro <74827509+strozilla@users.noreply.github.com>
2021-07-12 06:52:33 -0500

    practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div />;
+  const [count, newCount] = useState(() => {
+    return 0;
+  });
+
+  function increase() {
+    newCount(count + 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+    </div>
+  );
 }
 
 export default App;
